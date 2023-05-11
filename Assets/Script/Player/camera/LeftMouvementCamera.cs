c75198d41e0e3845a6fa0ba56f6e13447f90187e
emil <ehp84@sfr.fr>
2023-05-11 16:56:10 +0200

    début projet : mouvement + camera

---
+++ Assets/Script/Player/camera/LeftMouvementCamera.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftMouvementCamera : MonoBehaviour
+{
+    [Header("camera")]
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject player;
+    [Header("vitesse")]
+    [SerializeField] private float Speed;
+
+    private bool _isOnContact;
+
+    private void FixedUpdate() {
+        if (_isOnContact == true) {
+            cam.transform.position -= new Vector3(1, 0, 0) * Speed * Time.deltaTime;
+            
+            if (cam.transform.position.x - player.transform.position.x < 0.001f) {
+                _isOnContact = false;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.name == "Player") {
+            _isOnContact = true;
+        }
+    }
+}
---
+++ Assets/Script/Player/mouvement/PlayerMouvement.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float moveSpeed = 10f;
+    
+    private float _moveDir;
+    
+    private bool  _jumpPressed;
+    private float _jumpYVel;
+    
+    private Vector3 _moveVel;
+    
+    private bool IsGrounded;
+    
+    private void Update()
+    {
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        HandleJump();
+    }
+
+    private void HandleJump() {
+        if (IsGrounded && _jumpPressed) {
+            _jumpYVel = CalculateJumpVel(jumpHeight);
+            _jumpPressed = false;
+            
+            _moveVel = _rigidbody2D.velocity;
+            _moveVel.y = _jumpYVel;
+            _rigidbody2D.velocity = _moveVel;
+        }
+    }
+
+    private void Move() {
+        _moveVel = _rigidbody2D.velocity;
+        _moveVel.x = _moveDir * moveSpeed *Time.fixedDeltaTime;
+        _rigidbody2D.velocity = _moveVel;
+    }
+    
+
+    private float CalculateJumpVel(float height) {
+        return MathF.Sqrt((-2 * _rigidbody2D.gravityScale*Physics2D.gravity.y * height));
+    }
+
+    void GetInput() {
+        _moveDir = Input.GetAxisRaw("Horizontal"); 
+        _jumpPressed |= Input.GetKeyDown(KeyCode.Space); 
+    }
+    
+    private void OnCollisionEnter2D(Collision2D col) {
+        IsGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        IsGrounded = false;
+    }
+}
